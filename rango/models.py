05e35b6ca1aaff210687c9a11aaa6a342e744733
Xueying Qin <2335466q@student.gla.ac.uk>
2018-01-21 16:06:58 +0000

    Chapter5 Create models

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# Category model
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+# Page model
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    class Meta:
+        verbose_name_plural = 'Pages'
+
+    def __str__(self):
+        return self.title
