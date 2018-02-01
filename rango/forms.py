5bebfc7b327bbdb0c3d4090303a613350d57369a
Xueying Qin <2335466q@student.gla.ac.uk>
2018-02-01 01:03:40 +0000

    Chapter7 Create Forms

--- rango/forms.py
+++ rango/forms.py
@@ -14,10 +14,19 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the title of page.")
+    title = forms.CharField(max_length=128, help_text="Please enter the title of page.")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
         model = Page
-        exclude = ('Category', )
+        exclude = ('category',)
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = self.cleaned_data.get('url')
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+            return cleaned_data
+
