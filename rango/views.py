fd1669689688bb8081cc1fccd4ec7355e369cd0e
Xueying Qin <2335466q@student.gla.ac.uk>
2018-02-04 01:12:52 +0000

    Chapter8 Working with Templates

--- rango/views.py
+++ rango/views.py
@@ -16,6 +16,8 @@ def index(request):
 
 
 def about(request):
+    print(request.method)
+    print(request.user)
     return render(request, 'rango/about.html')
     # return HttpResponse("Rango says here is the about page! <br/> <a href='/rango/'> Index </a>")
 
